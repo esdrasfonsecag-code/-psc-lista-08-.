5510a32dfecf3b72f9c06f890bb9f1e186c31aec
Esdras <esdrasfonsecag@gmail.com>
2025-10-28 09:26:24 -0300

    SumThreeNumbers.java

---
+++ SumThreeNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int a = sc.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int b = sc.nextInt();
+        System.out.print("Digite o terceiro número: ");
+        int c = sc.nextInt();
+
+        int resultado = somaTresNumeros(a, b, c);
+        System.out.println("A soma dos três números é: " + resultado);
+
+        sc.close();
+    }
+
+    static int somaTresNumeros(int x, int y, int z) {
+        return x + y + z;
+    }
+}
